8da5813ff5297941e61bb6a80d97c4f9a92a14b2
cuiyingnan <18612526681@139.com>
2019-04-14 08:24:32 +0800

    update class test

---
+++ java/weixiao-ly/src/main/java/com/erparking/config/Constants.java
@@ -0,0 +1,17 @@
+package com.erparking.config;
+
+/**
+ * @Description
+ * @AUTHOR cuiyingnan
+ * @DATE 2019/4/14 7:30
+ **/
+public class Constants {
+    /**
+     * app key
+     */
+    public final static String appKey = "3F4A87608229F403";
+    /**
+     * 前16位
+     */
+    public final static String secretKey = "7264D046017897C7";
+}
