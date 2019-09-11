b8c75a161fde848db4fde44dae1f358f706d3196
cuiyingnan <18612526681@163.com>
2019-09-11 15:35:35 +0800

    ok

--- java/springcloud/eureka-provider/user-services/src/main/java/com/cc/service/UserImpl.java
+++ java/springcloud/eureka-provider/user-services/src/main/java/com/cc/service/UserImpl.java
@@ -4,6 +4,8 @@ import com.cc.api.UserApi;
 import com.cc.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,16 +19,17 @@ import java.util.List;
 @Slf4j
 public class UserImpl implements UserApi {
     @Override
-    public List<User> getUsers() {
+    public List<User> getUsers(@PathVariable("id") String id) {
+        System.out.println("here is run");
         List list = new ArrayList();
-        list.add(new User("飞天", "18612526681"));
+        list.add(new User(" 传入 ID是：", id));
         list.add(new User("入地", "18612526681"));
         list.add(new User("封神", "18612526681"));
         return list;
     }
 
     @Override
-    public boolean saveUser(User user) {
+    public boolean saveUser(@RequestBody User user) {
         log.info("保存用户成功", user);
         return true;
     }
