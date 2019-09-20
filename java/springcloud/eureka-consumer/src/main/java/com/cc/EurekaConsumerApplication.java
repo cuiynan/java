59df70489555159cdda279fb90d840f04bda0382
cuiyingnan <18612526681@163.com>
2019-09-20 15:06:18 +0800

    ok

--- java/springcloud/eureka-consumer/src/main/java/com/cc/EurekaConsumerApplication.java
+++ java/springcloud/eureka-consumer/src/main/java/com/cc/EurekaConsumerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * 当前使用feign进行服务的调用，并未实现负载
+ * 在项目practis中，使用ribbon+resttemplate已实现负载，但过度依赖配置文件中的调用地址
+ * 故:
+ * 需要将feign改造，不依赖调用 地址进行。
+ *
  * @author cuiyingnan
  */
 @EnableFeignClients(basePackages = "com")
