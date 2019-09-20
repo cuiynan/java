9bd8413703aa36531d95adbd70ede7bacc2f42d1
cuiyingnan <18612526681@163.com>
2019-09-20 15:05:59 +0800

    调整为使用feign+nginx进行负载。

--- java/springcloud/eureka-provider/user-api/src/main/java/com/cc/api/UserApi.java
+++ java/springcloud/eureka-provider/user-api/src/main/java/com/cc/api/UserApi.java
@@ -9,9 +9,24 @@ import java.util.List;
 /**
  * @author : cuiyingnan
  * @date : 2019/8/29 17:18
- * @desc :
+ * @desc : 这里使用nginx + url 进行集群的负载均衡
+ * 非常爽。
+ * 1，运行起来后，需要在本地配置url 对应的host。
+ * 2，配置nginx www.myurl.com对应的集群上点，如：
+ * nginx.conf配置以下信息
+ * upstream myproject {
+ * server 127.0.0.1:9999 weight=3;
+ * server 127.0.0.1:9002;
+ * }
+ * <p>
+ * location / {
+ * proxy_pass http://myproject;
+ * }
  */
-@FeignClient(value = "user-svc", url = "user-api:9001/")
+//第一种对应单机的时候使用
+//@FeignClient(value = "user-svc", url = "user-api:9001/")
+//下方可使用url进行集群
+@FeignClient(value = "user-svc", url = "www.myurl.com")
 @RestController
 public interface UserApi {
 
