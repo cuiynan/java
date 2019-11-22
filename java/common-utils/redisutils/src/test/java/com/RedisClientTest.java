27e87e70c9f7be1993dcba6135d46f7fac6b69b3
cuiyingnan <18612526681@163.com>
2019-11-22 09:52:33 +0800

    removeUnusedFiles

--- java/common-utils/redisutils/src/test/java/com/RedisClientTest.java
+++ java/common-utils/redisutils/src/test/java/com/RedisClientTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : cuiyingnan
  * @date : 2019/11/21 15:17
@@ -22,10 +25,19 @@ public class RedisClientTest {
     @Test
     public void get() {
         UserTest userTest = UserTest.builder().username("1234").password("234").build();
-        redisClient.set("user", userTest);
-        Object obj = redisClient.get("user");
-        if (obj instanceof UserTest){
-            UserTest result = (UserTest)obj;
+        List list = new ArrayList<>();
+        list.add(userTest);
+        list.add(userTest);
+        list.add(userTest);
+
+        redisClient.lSet("1", list);
+
+        long a = redisClient.lGetListSize("1");
+        Object obj = redisClient.lGet("1", 0, a);
+        if (obj instanceof List) {
+
+            List result = (List) obj;
+
             System.out.println(result);
         }
     }
